197969f1c455b5c0a0f51dd580ffdddc532a1628
Matheus Baptista <matheushbm192@gmail.com>
2025-06-03 07:37:38 -0300

    Adicionando mustache.js no projeto

--- backend/src/controllers/petController.ts
+++ backend/src/controllers/petController.ts
@@ -5,7 +5,10 @@ const petDao = new PetDAO()
 
 const getAllPets = async (req: Request, res: Response) => {
     try {
+        //Aqui deve ser chamada a RN e só apartir da RN a DAO é chamada
         const pets = await petDao.getAllPets()
+        //O retorno deve ser em HTML
+        //utilizar muustache para montar html
         res.json(pets)
     } catch (error: any) {
         res.status(500).json({ error: error.message })
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,16 @@
 import express, { application } from 'express';
 import petRoutes from "./routes/petRoutes"
-
+import mustacheExpress from 'mustache-express'
+import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Configura o engine Mustache
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+
+// Define onde estão os templates
+app.set('views', path.join(__dirname, 'pages'));
 
 app.use(express.json());
 app.use('/api', petRoutes)
