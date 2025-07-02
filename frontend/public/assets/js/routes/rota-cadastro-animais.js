71e181d8f5a57daf28d017ce87557bbb1a6b8b01
paolavit <paolaqq0@icloud.com>
2025-07-02 12:48:12 -0300

    atualizando

--- frontend/public/assets/js/routes/rota-cadastro-animais.js
+++ frontend/public/assets/js/routes/rota-cadastro-animais.js
@@ -15,7 +15,6 @@ export function initializeCadastroPage() {
         }
     }, 100);
 }
-//import { exibirMensagemTemporaria } from '../cadastro_animais';
 function enviarCadastro() {
     const form = document.getElementById('formulario-cadastro-animal');
     const button = document.getElementById('btn-cadastrar');
@@ -60,24 +59,4 @@ function enviarCadastro() {
         button.disabled = false;
         button.textContent = 'Cadastrar Animal';
     });
-    /*fetch('http://localhost:3000/api/petsPost', {
-      method: 'POST',
-      body: formData,
-    })
-      .then(res => {
-        if (!res.ok) throw new Error('Erro no envio');
-        return res.text();
-      })
-      .then((res) => {
-        console.log(res);
-        exibirMensagemTemporaria('mensagem');
-        form.reset();
-      })
-      .catch(() => {
-        exibirMensagemTemporaria('mensagemErro');
-      })
-      .finally(() => {
-        button.disabled = false;
-        button.textContent = 'Cadastrar Animal';
-      });*/
 }
