965f0ca667f437b2181f70be9df23716df78092a
Matheus <matheushsbaeta@gmail.com>
2025-08-03 19:57:39 -0300

    server e routes

--- backend/src/routes/animaisAdotadosRoutes.ts
+++ backend/src/routes/animaisAdotadosRoutes.ts
@@ -5,6 +5,7 @@ import { autenticarToken } from '../utils/auth'
 const animaisAdotadosCTR = new AnimaisAdotadosCTR() 
 const router = Router()
 
+// autenticar token 
 router.get('/', autenticarToken, animaisAdotadosCTR.getAnimaisAdotadosByUsuarioId);
 
 export default router
